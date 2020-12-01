ecadcee833df3ec1e87017c425a6296f49be7718
andreeanen <andreeanen@yahoo.com>
2020-12-01 15:08:54 +0100

    Rename and clean code CheckFreeDate()

--- Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
@@ -19,10 +19,10 @@ namespace TollFeeCalculator
             for(int i = 0; i < dates.Length; i++) {
                 dates[i] = DateTime.Parse(datesCSV[i]);
             }
-            Console.Write("The total fee for the inputfile is: " + TotalFeeCost(dates));
+            Console.Write("The total fee for the inputfile is: " + CalculateTotalFee(dates));
         }
 
-        static int TotalFeeCost(DateTime[] dates) 
+        static int CalculateTotalFee(DateTime[] dates) 
         {
             int fee = 0;
             int multiPassageIntervalInMinutes = 60;
@@ -31,36 +31,50 @@ namespace TollFeeCalculator
             {
                 int differenceInMinutes = (date - initialInvervalDate).Minutes;
                 if(differenceInMinutes > multiPassageIntervalInMinutes) {
-                    fee += TollFeePass(date);
+                    fee += CalculateFeePerTimespan(date);
                     initialInvervalDate = date;
                     Console.WriteLine($"fee for {date} is {fee}" );
                 } else {
-                    fee += Math.Max(TollFeePass(date), TollFeePass(initialInvervalDate));
+                    fee += Math.Max(CalculateFeePerTimespan(date), CalculateFeePerTimespan(initialInvervalDate));
                     Console.WriteLine($"fee for (else) {date.Hour}:{date.Minute} is {fee}");
                 }
             }
             return Math.Max(fee, 60); //bugg
         }
 
-        static int TollFeePass(DateTime d)
+        static int CalculateFeePerTimespan(DateTime date)
         {
-            if (free(d)) return 0;
-            int hour = d.Hour;
-            int minute = d.Minute;
-            if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-            else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-            else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-            else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-            else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-            else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-            else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-            else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+            if (CheckFreeDate(date)) return 0; //bugg
+
+            int hour = date.Hour;
+            int minute = date.Minute;
+            if (hour == 6 && minute >= 0 && minute <= 29) 
+                return 8;
+            else if (hour == 6 && minute >= 30 && minute <= 59) 
+                return 13;
+            else if (hour == 7 && minute >= 0 && minute <= 59) 
+                return 18;
+            else if (hour == 8 && minute >= 0 && minute <= 29) 
+                return 13;
+            else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) 
+                return 8;
+            else if (hour == 15 && minute >= 0 && minute <= 29) 
+                return 13;
+            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) 
+                return 18; //bugg
+            else if (hour == 17 && minute >= 0 && minute <= 59) 
+                return 13;
+            else if (hour == 18 && minute >= 0 && minute <= 29) 
+                return 8;
             else return 0;
         }
         //Gets free dates
-        static bool free(DateTime day) {
-        return (int)day.DayOfWeek == 5 || (int)day.DayOfWeek == 6 || day.Month == 7;
+        static bool CheckFreeDate(DateTime date) 
+        {
+            int saturday = 6;
+            int sunday = 0;
+            int july = 7;
+            return (int)date.DayOfWeek == saturday || (int)date.DayOfWeek == sunday || date.Month == july; //bugg 
         }
     }
 }
