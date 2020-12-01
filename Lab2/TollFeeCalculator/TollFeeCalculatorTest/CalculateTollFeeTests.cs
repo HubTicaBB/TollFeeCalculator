609dad363abaeb9c511e285884a736eb88a5881b
HubTicaBB <tijana.ilic87@protonmail.com>
2020-12-01 16:47:44 +0100

    Create Parse method test to check if number of dates in inputData argument are same as output

---
+++ Lab2/TollFeeCalculator/TollFeeCalculatorTest/CalculateTollFeeTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TollFeeCalculator;
+
+namespace TollFeeCalculatorTest
+{
+    [TestClass]
+    public class CalculateTollFeeTests
+    {
+        [TestMethod]
+        public void Parse_WhenCalled_ReturnSameLength()
+        {
+            var inputData = "2020-06-30 00:05, 2020-06-30 06:34";
+
+            DateTime[] actual = Program.Parse(inputData);
+            DateTime[] expected = new DateTime[]
+            {
+                new DateTime(2020, 6, 30, 0, 5, 0),
+                new DateTime(2020, 6, 30, 6, 34, 0)
+            };
+
+            Assert.AreEqual(expected.Length, actual.Length);
+        }
+    }
+}
